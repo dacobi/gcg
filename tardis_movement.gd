d7c3241891a4d23b8f3faddf75b2812f81bf1db5
Jacob Kolding <dacobi@gmail.com>
2026-06-01 11:07:04 +0200

    Tardis main scene 1

---
+++ tardis_movement.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@export var speed: float = 20.0
+@export var spawn_position: Vector3 = Vector3(-45, 45, 0)
+@export var right_boundary: float = 45.0
+
+var tardis_scene = preload("res://tardis.tscn")
+var active_tardis: Node3D = null
+
+@onready var audio_player = $TardisAudio
+
+func _on_spawn_tardis():
+	if active_tardis != null:
+		return # Tardis already active
+
+	active_tardis = tardis_scene.instantiate()
+	active_tardis.position = spawn_position
+	add_child(active_tardis)
+	
+	if audio_player:
+		audio_player.play()
+
+func _process(delta: float) -> void:
+	if active_tardis != null:
+		active_tardis.position.x += speed * delta
+		
+		if active_tardis.position.x > right_boundary:
+			active_tardis.queue_free()
+			active_tardis = null
+			if audio_player:
+				audio_player.stop()
