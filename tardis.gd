c0738c3f23d35e1434b5bb3a4a29fcb9a9f407f9
Jacob Kolding <dacobi@gmail.com>
2026-06-01 08:30:32 +0200

    Tardis 1

---
+++ tardis.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+var rotation_speed = 2.0
+
+@onready var visuals = get_node_or_null("Visuals")
+@onready var spotlight_container = get_node_or_null("Visuals/Lamp/SpotLightContainer")
+
+func _process(delta: float) -> void:
+	# Rotate the visuals around the local Y axis (preserving the root's tilt)
+	if visuals:
+		visuals.rotate_object_local(Vector3.UP, rotation_speed * delta)
+	
+	# Rotate the spotlight independently
+	if spotlight_container:
+		spotlight_container.rotate_y(4.0 * delta)
