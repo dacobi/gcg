f22c9b3f4b832a8c9882c0428ddc77aed08f6916
Jacob Kolding <dacobi@gmail.com>
2026-06-01 10:02:31 +0200

    Tardis 4

--- tardis.gd
+++ tardis.gd
@@ -3,7 +3,7 @@ extends Node3D
 var rotation_speed = 2.0
 
 @onready var visuals = get_node_or_null("Visuals")
-@onready var spotlight_container = get_node_or_null("Visuals/Lamp/SpotLightContainer")
+@onready var spotlight_container = get_node_or_null("Visuals/Structure/Lamp/SpotLightContainer")
 
 func _process(delta: float) -> void:
 	# Rotate the visuals around the local Y axis (preserving the root's tilt)
