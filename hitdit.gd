5c510ad0fe7447cd6067e38991aed9c5f338b822
Jacob Kolding <dacobi@gmail.com>
2026-06-07 10:20:03 +0200

    bunker die fix

--- hitdit.gd
+++ hitdit.gd
@@ -1,18 +1,22 @@
 extends Area3D
 
 var is_bunker = true
+var is_dead = false
 
 func _ready():
 	area_entered.connect(_on_body_entered)
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
+	if is_dead: return
 	# Ignore self-collisions with other adjacent pixels in the grid
 	if body == self or body.get_script() == self.get_script():
 		return
-	queue_free()
+	die()
 
 func die()->void:
+	if is_dead: return
+	is_dead = true
 	var audio = get_node_or_null("../BunkerExplosions")
 	if audio:
 		audio.play()
